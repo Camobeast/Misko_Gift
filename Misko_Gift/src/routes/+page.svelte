e704a6a695cbc054ef10163ae941b630311f766f
Camobeast <jakub.lilley@gmail.com>
2023-12-28 22:33:30 +0100

    Project redone, basic style applied

--- Misko_Gift/src/routes/+page.svelte
+++ Misko_Gift/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let name = 'Miško';
+</script>
+
+<h1 class="h1 text-red-500 py-5 text-center font-bold">Merry Christmas {name}!</h1>
+<h2 class="h2 text-red-500 py-5 text-center underline font-semibold">Bellow you can find the picture of your present!</h2>
+
+<div class="card p-4" id="present">
+	<img class="w-full" src="zidle.png" alt="Miskuv present - a chair">
+</div>
