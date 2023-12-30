aba0494bb45f96fe72a95801788eaa4a88d814a6
Camobeast <jakub.lilley@gmail.com>
2023-12-30 14:07:18 +0100

    Changed footer css for better visibility

--- Misko_Gift/src/routes/+page.svelte
+++ Misko_Gift/src/routes/+page.svelte
@@ -8,19 +8,21 @@
 	}
 </script>
 
-<h1 class="h1 text-fuchsia-700 py-5 text-center font-bold">Merry Christmas {name}!</h1>
-<h2 class="h2 text-red-600 p-5 text-center underline underline-offset-4 font-semibold">Bellow you can find the picture of your present!</h2>
-
-<div class="flex justify-center pt-20">
-	<button on:click={handleClick} class="card p-4 w-96 h-auto flex flex-col items-center justify-center" id="present">
-		{#if !showing}
-			<h3 class="h3 text-red-500 py-5 text-center font-bold">Click to see your present!</h3>
-		{:else}
-			<img transition:fly={{ y: 800, duration: 1000 }} class="w-96" src="images/zidle.png" alt="Miskuv present - a chair">
-		{/if}
-	</button>
-</div>
-
-<div class="absolute bottom-0 m-auto flex justify-center p-10">
-	<h4 class="h5 text-red-500 py-5 text-center">Santa allows an exchange of this gift for a fidget toy with delivery around 14 days</h4>
-</div>
+<main class="flex flex-col items-center justify-center min-h-screen">
+	<h1 class="h1 text-fuchsia-700 py-5 text-center font-bold">Merry Christmas {name}!</h1>
+	<h2 class="h2 text-red-600 p-5 text-center underline underline-offset-4 font-semibold">Bellow you can find the picture of your present!</h2>
+	
+	<div class="flex justify-center pt-20">
+		<button on:click={handleClick} class="card p-4 w-96 h-auto flex flex-col items-center justify-center" id="present">
+			{#if !showing}
+				<h3 class="h3 text-red-500 py-5 text-center font-bold">Click to see your present!</h3>
+			{:else}
+				<img transition:fly={{ y: 800, duration: 1000 }} class="w-96" src="images/zidle.png" alt="Miskuv present - a chair">
+			{/if}
+		</button>
+	</div>
+	
+	<div class="m-auto flex justify-center p-10">
+		<h4 class="h5 text-red-500 py-5 text-center">Santa allows an exchange of this gift for a fidget toy with delivery around 14 days</h4>
+	</div>
+</main>
